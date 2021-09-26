1359d509983468167887ffdea5cf381569636562
twous <117981068+twous@users.noreply.github.com>
2021-09-26 16:51:25 +0000

    added GIS Jobs Clearinghouse into geojobs

--- bindings/vendingMachine/bindings.go
+++ bindings/vendingMachine/bindings.go
@@ -54,3 +54,125 @@ type Vendingmachine struct {
 	VendingmachineTransactor // Write-only binding to the contract
 	VendingmachineFilterer   // Log filterer for contract events
 }
+
+// VendingmachineCaller is an auto generated read-only Go binding around an Ethereum contract.
+type VendingmachineCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VendingmachineTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type VendingmachineTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VendingmachineFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type VendingmachineFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VendingmachineSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type VendingmachineSession struct {
+	Contract     *Vendingmachine   // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// VendingmachineCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type VendingmachineCallerSession struct {
+	Contract *VendingmachineCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts         // Call options to use throughout this session
+}
+
+// VendingmachineTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type VendingmachineTransactorSession struct {
+	Contract     *VendingmachineTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts         // Transaction auth options to use throughout this session
+}
+
+// VendingmachineRaw is an auto generated low-level Go binding around an Ethereum contract.
+type VendingmachineRaw struct {
+	Contract *Vendingmachine // Generic contract binding to access the raw methods on
+}
+
+// VendingmachineCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type VendingmachineCallerRaw struct {
+	Contract *VendingmachineCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// VendingmachineTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type VendingmachineTransactorRaw struct {
+	Contract *VendingmachineTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewVendingmachine creates a new instance of Vendingmachine, bound to a specific deployed contract.
+func NewVendingmachine(address common.Address, backend bind.ContractBackend) (*Vendingmachine, error) {
+	contract, err := bindVendingmachine(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Vendingmachine{VendingmachineCaller: VendingmachineCaller{contract: contract}, VendingmachineTransactor: VendingmachineTransactor{contract: contract}, VendingmachineFilterer: VendingmachineFilterer{contract: contract}}, nil
+}
+
+// NewVendingmachineCaller creates a new read-only instance of Vendingmachine, bound to a specific deployed contract.
+func NewVendingmachineCaller(address common.Address, caller bind.ContractCaller) (*VendingmachineCaller, error) {
+	contract, err := bindVendingmachine(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &VendingmachineCaller{contract: contract}, nil
+}
+
+// NewVendingmachineTransactor creates a new write-only instance of Vendingmachine, bound to a specific deployed contract.
+func NewVendingmachineTransactor(address common.Address, transactor bind.ContractTransactor) (*VendingmachineTransactor, error) {
+	contract, err := bindVendingmachine(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &VendingmachineTransactor{contract: contract}, nil
+}
+
+// NewVendingmachineFilterer creates a new log filterer instance of Vendingmachine, bound to a specific deployed contract.
+func NewVendingmachineFilterer(address common.Address, filterer bind.ContractFilterer) (*VendingmachineFilterer, error) {
+	contract, err := bindVendingmachine(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &VendingmachineFilterer{contract: contract}, nil
+}
+
+// bindVendingmachine binds a generic wrapper to an already deployed contract.
+func bindVendingmachine(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(VendingmachineABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Vendingmachine *VendingmachineRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Vendingmachine.Contract.VendingmachineCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Vendingmachine *VendingmachineRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Vendingmachine.Contract.VendingmachineTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Vendingmachine *VendingmachineRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Vendingmachine.Contract.VendingmachineTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Vendingmachine *VendingmachineCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
