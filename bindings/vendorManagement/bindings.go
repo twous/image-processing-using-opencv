66a2b5b6540d7bf2cae8278ba12d27ac96ce3b93
twous <117981068+twous@users.noreply.github.com>
2022-06-26 19:59:51 +0000

    0.1.43

--- bindings/vendorManagement/bindings.go
+++ bindings/vendorManagement/bindings.go
@@ -368,4 +368,9 @@ func (_Vendormanagement *VendormanagementSession) RegisterProduct(_name string, 
 //
 // Solidity: function registerProduct(string _name, string[] _locations, uint256 _cost) returns(bool)
 func (_Vendormanagement *VendormanagementTransactorSession) RegisterProduct(_name string, _locations []string, _cost *big.Int) (*types.Transaction, error) {
-	return _Vendormanagement.Contract.RegisterProduct(&_Vendormanagement.TransactOpts, _name, _locations, 
+	return _Vendormanagement.Contract.RegisterProduct(&_Vendormanagement.TransactOpts, _name, _locations, _cost)
+}
+
+// RemoveProductLocation is a paid mutator transaction binding the contract method 0x5d85ed13.
+//
+// Solidi
