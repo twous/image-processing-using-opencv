99cf784a34faec150dac6ab0b5867261f86d04f2
twous <117981068+twous@users.noreply.github.com>
2022-08-22 19:26:38 +0000

    adds

--- bindings/vendorManagement/bindings.go
+++ bindings/vendorManagement/bindings.go
@@ -502,4 +502,8 @@ func (_Vendormanagement *VendormanagementFilterer) FilterProductLocationAdded(op
 // WatchProductLocationAdded is a free log subscription operation binding the contract event 0x20eca5c8f895538bd583d98f6dc289ffd029bf3d0a3b5362dffeace9ab6634dc.
 //
 // Solidity: event ProductLocationAdded(string _name, string _location)
-func (_Vendormanagement *VendormanagementFilterer) WatchProductLocationAdded(opts *bind.WatchOpts, sink chan<- *VendormanagementProductLocationAdded) (event.Sub
+func (_Vendormanagement *VendormanagementFilterer) WatchProductLocationAdded(opts *bind.WatchOpts, sink chan<- *VendormanagementProductLocationAdded) (event.Subscription, error) {
+
+	logs, sub, err := _Vendormanagement.contract.WatchLogs(opts, "ProductLocationAdded")
+	if err != nil {
+		retu
