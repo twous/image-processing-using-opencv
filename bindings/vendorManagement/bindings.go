ae77819540a9566cf6a76bc54772cb9621207b9e
twous <117981068+twous@users.noreply.github.com>
2022-08-29 17:02:52 +0000

    further cleaning

--- bindings/vendorManagement/bindings.go
+++ bindings/vendorManagement/bindings.go
@@ -536,4 +536,7 @@ func (_Vendormanagement *VendormanagementFilterer) WatchProductLocationAdded(opt
 	}), nil
 }
 
-// ParseProductLocationAdded is a log parse operation binding the contract event 0x20eca5c8f895538bd583d98f6dc289ffd029bf3d0a3b5362dffeace9ab66
+// ParseProductLocationAdded is a log parse operation binding the contract event 0x20eca5c8f895538bd583d98f6dc289ffd029bf3d0a3b5362dffeace9ab6634dc.
+//
+// Solidity: event ProductLocationAdded(string _name, string _location)
+func (_Vendormanagement *VendormanagementFilterer) Par
