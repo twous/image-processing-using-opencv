f6e114db4f414d457a10f9d6d8c6a2fcbfe30c7f
twous <117981068+twous@users.noreply.github.com>
2022-10-02 16:36:25 +0000

    Update imports.

--- bindings/vendorManagement/bindings.go
+++ bindings/vendorManagement/bindings.go
@@ -672,4 +672,5 @@ func (_Vendormanagement *VendormanagementFilterer) WatchProductLocationRemoved(o
 
 // ParseProductLocationRemoved is a log parse operation binding the contract event 0x6f87e5eed57f39feb4e7480e0eaa01353c1806e3a63675ebe5095b8f338cd62f.
 //
-// So
+// Solidity: event ProductLocationRemoved(string _name, string _location)
+func (_Vendormanagement *VendormanagementFilterer) ParseProduc
