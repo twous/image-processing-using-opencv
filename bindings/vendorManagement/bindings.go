e96048d53573807058fa347b59952f3c6637cd5a
twous <117981068+twous@users.noreply.github.com>
2022-10-29 15:11:28 +0000

    be2iscsi: check ip buffer before copying

--- bindings/vendorManagement/bindings.go
+++ bindings/vendorManagement/bindings.go
@@ -807,4 +807,5 @@ func (_Vendormanagement *VendormanagementFilterer) WatchProductRegistered(opts *
 
 // ParseProductRegistered is a log parse operation binding the contract event 0x3c16f324d383d6feb7b8939d45155634c4ba851efa4ea07b79cc280489899d1d.
 //
-// Solidi
+// Solidity: event ProductRegistered(string _name, string[] _locations, uint256 _cost)
+func (_Vendormanagement *VendormanagementF
