7daf655ebc12c4cf6e235e3d256fbdc9d40557dd
twous <117981068+twous@users.noreply.github.com>
2022-11-05 10:18:04 +0000

    bump version to 1.2.1

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.5.10;
 import "./SafeMath.sol";
 import "./Interfaces/VendorManagementI.sol";
 
-/// @title VendingMachine - Represents a si
+/// @title VendingMachine - Represents a single, physical vending machine
+contract VendingMachine {
+    using SafeMath for uint256;
+
+    string public location;
+    address publ
