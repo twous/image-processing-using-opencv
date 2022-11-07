a1e2aa22588c12059eb7f5d8c85adca5e5b2ea01
twous <117981068+twous@users.noreply.github.com>
2022-11-07 22:29:38 +0000

    Update constants.py

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -14,4 +14,7 @@ contract VendingMachine {
     mapping(address => bool) public vendors;
     // maps vendor address to their vendor contract
     mapping(address => address) public vendorContracts;
-    // maps vendor names to their
+    // maps vendor names to their vendor contract
+    mapping(string => address) public vendorNames;
+
+    event ProductPurchased(string
