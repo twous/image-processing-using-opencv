c23defc9cc75bf6949e1a3d2e8e642041b61992e
twous <117981068+twous@users.noreply.github.com>
2022-11-24 11:23:01 +0000

    Add mock thumbnail

--- contracts/VendorFactory.sol
+++ contracts/VendorFactory.sol
@@ -2,4 +2,7 @@ pragma solidity ^0.5.10;
 
 import "./VendorManagement.sol";
 
-/// @title VendorFac
+/// @title VendorFactory - Used to deploy vendor contracts
+contract VendorFactory {
+
+    // array of all vendor contracts regist
