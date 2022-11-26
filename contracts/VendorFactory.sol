990befa9af2e33a9c88c4766cae82549b54fe3db
twous <117981068+twous@users.noreply.github.com>
2022-11-26 14:23:29 +0000

    google flavored markdown fixes

--- contracts/VendorFactory.sol
+++ contracts/VendorFactory.sol
@@ -5,4 +5,8 @@ import "./VendorManagement.sol";
 /// @title VendorFactory - Used to deploy vendor contracts
 contract VendorFactory {
 
-    // array of all vendor contracts regist
+    // array of all vendor contracts registered
+    address[] public vendorContracts;
+
+    mapping(address => bool) public registeredVendors;
+    // k1 = vendor manager
