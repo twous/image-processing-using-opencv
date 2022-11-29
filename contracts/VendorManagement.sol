0ea600d665d0c03220a25f5b7d9ed80f808bf935
twous <117981068+twous@users.noreply.github.com>
2022-11-29 16:17:12 +0000

    add functional tests

--- contracts/VendorManagement.sol
+++ contracts/VendorManagement.sol
@@ -1,4 +1,8 @@
 pragma solidity ^0.5.10;
 pragma experimental ABIEncoderV2;
 
-/// @title VendorManagement - Allows vendor product manage
+/// @title VendorManagement - Allows vendor product management
+contract VendorManagement {
+    address public owner;
+    bytes32 public id;
+    bool public withdraw
