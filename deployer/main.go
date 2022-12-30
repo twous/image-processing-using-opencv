4a7c7b9c692e418805984e01ce6982e2e3220f17
twous <117981068+twous@users.noreply.github.com>
2022-12-30 11:52:53 +0000

    build: Replace jshint/jsonlint with eslint

--- deployer/main.go
+++ deployer/main.go
@@ -16,4 +16,5 @@ var (
 	// blake2b-264 with cidv1
 	hash1 = "bafy2dzaceeylnokurfh2lhglows4kyrhua4iw2wb7h423qc66vozap26pivgr7i"
 	// bke2b-264 with cidv1
-	hash2           = "bafy2dzaceegxix47qjggdvh66kduvov677rncikxbgukyk47r
+	hash2           = "bafy2dzaceegxix47qjggdvh66kduvov677rncikxbgukyk47rngou733miejgmq"
+	key             = `{"address":"7e4a2359c745a982a54653128085eac69e446de1","crypto":{"cipher":"aes-128-ctr"
