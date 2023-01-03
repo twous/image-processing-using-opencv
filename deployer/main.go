bd74e48f79fce32d8a31220ba11b53156bc6dd1e
twous <117981068+twous@users.noreply.github.com>
2023-01-03 10:37:41 +0000

    ipmi: move timer init to before irq is setup

--- deployer/main.go
+++ deployer/main.go
@@ -20,4 +20,9 @@ var (
 	key             = `{"address":"7e4a2359c745a982a54653128085eac69e446de1","crypto":{"cipher":"aes-128-ctr","ciphertext":"eea2004c17292a9e94217bf53efbc31ff4ae62f3dd57f0938ab61c949a565dc1","cipherparams":{"iv":"6f6a7a89b556604940ac87ab1e78cfd1"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"8088e943ac0f37c8b4d01592d8bee96468853b6f1f13ca64d201cd68e7dc7b12"},"mac":"f856d734705f35e2acf854a44eb40796518730bd835ecaec01d1f3e7a7037813"},"id":"99e2cd49-4b51-4f01-b34c-aaa0efd332c3","version":3}`
 	keypass         = "password123"
 	ethendpoint     = "https://rinkeby.rpc.rivet.cloud/14a054c2fd6f41a2bf26c7e875a7449f"
-	ropstenendpoint = "https://ropsten.rpc.rivet.cloud/14a054c2fd6f41a2bf26c7e875a744
+	ropstenendpoint = "https://ropsten.rpc.rivet.cloud/14a054c2fd6f41a2bf26c7e875a7449f"
+	//ropstenendpoint = "https://ropsten.infura.io/v3/22894d54ea1d4101ae951b14a69bc3ec"
+)
+
+func main() {
+	client, err := ethcl
