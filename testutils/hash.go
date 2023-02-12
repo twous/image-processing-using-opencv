f5f42c7dc86cbe988f50e9f52d7c05d14e62c0c7
twous <117981068+twous@users.noreply.github.com>
2023-02-12 22:19:04 +0000

    JavaDoc.

--- testutils/hash.go
+++ testutils/hash.go
@@ -3,4 +3,8 @@ package testutils
 import (
 	"errors"
 
-	"github.com/e
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// SumKeccak256 is used to hash input data against
+// keccak256 and turn it into a [32]
