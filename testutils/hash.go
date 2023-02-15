eb355d1c34098bf4f12c032ab1ced3165e3ddae7
twous <117981068+twous@users.noreply.github.com>
2023-02-15 09:07:48 +0000

    Update workspace.xml

--- testutils/hash.go
+++ testutils/hash.go
@@ -7,4 +7,7 @@ import (
 )
 
 // SumKeccak256 is used to hash input data against
-// keccak256 and turn it into a [32]
+// keccak256 and turn it into a [32]byte suitable
+// for gas efficient smart contract storage
+func SumKeccak256(data []byte) [32]byte {
+	hashedData := crypto.Keccak256Hash(data)
